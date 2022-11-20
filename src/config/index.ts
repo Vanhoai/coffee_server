3e52434e1bbfab301d34e9a60d114b727e7d5bf4
Hoai Aurora <tvhoai241223@gmail.com>
2022-11-21 00:44:56 +0700

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,17 @@
 export const getConfig = () => {
-    const DEBUG = true;
+    const DEBUG = false;
+
     const config = {
+        LOCAL: DEBUG,
         POSTGRES_HOST: DEBUG ? '127.0.0.1' : 'database',
         POSTGRES_PORT: DEBUG ? 5432 : 5434,
         POSTGRES_USER: 'postgres',
         POSTGRES_PASSWORD: DEBUG ? 'Tranhoai241223' : 'postgres',
         POSTGRES_DB: 'coffee',
         PORT: DEBUG ? 8080 : process.env.PORT,
+        POSTGRES_URI: DEBUG
+            ? 'postgres://username:password@hostname/databasename'
+            : 'postgres://sweqjour:QMdhbPmLX5v26h8ULPzlIiTlY1CuUan9@tiny.db.elephantsql.com/sweqjour',
         API: '/api/v1',
         COR_ORIGIN: DEBUG ? 'http://localhost:3000' : 'https://coffee-shop-frontend.vercel.app',
         ACCESS_TOKEN_SECRET: 'ACCESS_TOKEN_SECRET',
